2612a49952170c076a5ef6da15041e06b24179ff
afzal75 <afzalbhuiyan16@gmail.com>
2023-08-01 01:15:25 +0600

    github authentication successfully

---
+++ src/pages/login.js
@@ -0,0 +1,32 @@
+import RootLayout from "@/components/layout/RootLayout";
+import React from "react";
+import { signIn } from "next-auth/react";
+import Heading from "@/components/ui/Heading";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Heading title={"Login"} />
+      <div className="flex flex-col items-center justify-center md:h-[458px] bg-gray-100">
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        <button
+          className="bg-gray-800 hover:bg-gray-900 text-white px-4 py-2 rounded-md"
+          onClick={() =>
+            signIn("github", {
+              callbackUrl: "http://localhost:3000/",
+            })
+          }
+        >
+          Sign In with GitHub
+        </button>
+
+      </div>
+    </>
+  );
+};
+
+LoginPage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
+
+export default LoginPage;
